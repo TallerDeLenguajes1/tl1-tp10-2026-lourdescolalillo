fc7065263d106bd50c7bc7dc4f6441fd3a55678b
lourdes colalillo <lourdescolalillo@gmail.com>
2026-06-24 19:50:12 -0300

    ejercicio 4 terminado

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -5,6 +5,29 @@ class Program
 {
     static async Task Main()
     {
-        await AccesoDatos.GetTareas();
+        List<Tareas> lista = await AccesoDatos.GetTareas();
+        Console.WriteLine("\n====LISTADO DE TAREAS====");
+        Console.WriteLine("====TAREAS PENDIENTES====");
+        foreach (var tar in lista)
+        {
+            if (tar.Completed == false)
+            {
+                Console.WriteLine($"TITULO: {tar.Title}");
+                Console.WriteLine($"ESTADO: PENDIENTE");
+                Console.WriteLine("-----------------");
+            }
+
+        }
+
+        Console.WriteLine("====TAREAS COMPLETADAS====");
+        foreach (var tar in lista)
+        {
+            if (tar.Completed == true)
+            {
+                Console.WriteLine($"TITULO: {tar.Title}");
+                Console.WriteLine($"ESTADO: COMPLETADA");
+                Console.WriteLine("-----------------");
+            }
+        }
     }
 }
